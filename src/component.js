3474a9ed6777c572f9828846a4cfb2a8c96ee897
Twisted07 <iamtwisted07@gmail.com>
2024-01-11 14:06:14 +0100

    Form handling

--- src/component.js
+++ src/component.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
 ];
 
 
@@ -11,24 +13,52 @@ function Header() {
 }
 
 function Form() {
+    const [description, setDescription] = useState("");
+    const [quantity, setQuantity] = useState(1);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!description) return
+
+        const newItem = { description, quantity, id: Date.now(), packed: false };
+        console.log(newItem);
+
+        setDescription("");
+        setQuantity(1);
+    }
+
     return (
-        <div className="add-form">
+        <form className="add-form" onSubmit={handleSubmit}>
             <h3>What do you need for your trip? 😎</h3>
-        </div>
+            <select value={quantity} onChange={e => setQuantity(e.target.value)}>
+                {Array.from({length: 20}, (_, i) => i + 1).map(
+                    (num) => (
+                        <option value={num} key={num}>{num}</option>
+                    )
+                )}
+            </select>
+            <input type="text" placeholder="Item..." value={description} onChange={e => setDescription(e.target.value)} />
+            <button>Add</button>
+        </form>
     );
 }
 
 function List() {
     return (
-        <ul className="list">
-            {initialItems.map(item => <Item item={item.description} />)}
-        </ul>
+        <div className="list">
+            <ul>
+                {initialItems.map(item => <Item item={item} key={item.id}/>)}
+            </ul>
+        </div>
     );
 }
 
-function Item(props) {
+function Item({item}) {
     return (
-        <li>{props.item}</li>
+        <li>
+            <span style={item.packed ? {textDecoration: "line-through"} : {}}>{item.quantity} {item.description}</span>
+            <button>❌</button>
+        </li>
     );
 }
 
